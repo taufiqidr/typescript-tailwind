5a5f5fdc12580f791c3df0c54619920b852d12d7
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-11-06 11:18:40 +0800

    fixed navbar

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello world");
+const initApp = () => {
+    const hamburgerBtn = document.getElementById("hamburger-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const toggleMenu = () => {
+        mobileMenu === null || mobileMenu === void 0 ? void 0 : mobileMenu.classList.toggle("hidden");
+        mobileMenu === null || mobileMenu === void 0 ? void 0 : mobileMenu.classList.toggle("flex");
+        hamburgerBtn === null || hamburgerBtn === void 0 ? void 0 : hamburgerBtn.classList.toggle("toggle-btn");
+    };
+    hamburgerBtn === null || hamburgerBtn === void 0 ? void 0 : hamburgerBtn.addEventListener("click", toggleMenu);
+    mobileMenu === null || mobileMenu === void 0 ? void 0 : mobileMenu.addEventListener("click", toggleMenu);
+};
+document.addEventListener("DOMContentLoaded", initApp);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+const initApp = () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu?.classList.toggle("hidden");
+    mobileMenu?.classList.toggle("flex");
+    hamburgerBtn?.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn?.addEventListener("click", toggleMenu);
+  mobileMenu?.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
